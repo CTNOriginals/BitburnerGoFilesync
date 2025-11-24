28a1259aafe519f2dfd59f2aeaada1dcb7460e1f
CTN <info@ctn-originals.com>
2025-11-24 17:54:35 +0100

    Definitions: populated all definitions

--- filesync/rpc/definitions/definitions.go
+++ filesync/rpc/definitions/definitions.go
@@ -26,11 +26,87 @@ func (this Definitions) GetByMethod(method Method) Definition {
 
 var RPCDefinitions = Definitions{
 	{
+		Method:     GetFile,
+		Parameters: ParameterFields{"filename", "server"},
+		Response:   Response{Typ: ResString},
+	},
+	{
+		Method:     GetAllFiles,
+		Parameters: ParameterFields{"filename", "server"},
+		Response: Response{
+			Typ:     ResObject,
+			IsArray: true,
+			Fields: []ResponseField{
+				{Field: "filename", Typ: ResString},
+				{Field: "content", Typ: ResString},
+			},
+		},
+	},
+
+	{
 		Method:     GetFileNames,
 		Parameters: ParameterFields{"server"},
 		Response: Response{
 			Typ:     ResString,
 			IsArray: true,
+		},
+	},
+
+	{
+		Method:     GetFileMetadata,
+		Parameters: ParameterFields{"filename", "server"},
+		Response: Response{
+			Typ: ResObject,
+			Fields: []ResponseField{
+				{Field: "filename", Typ: ResString},
+				{Field: "atime", Typ: ResString},
+				{Field: "btime", Typ: ResString},
+				{Field: "mtime", Typ: ResString},
+			},
+		},
+	},
+	{
+		Method:     GetAllFileMetadata,
+		Parameters: ParameterFields{"server"},
+		Response: Response{
+			Typ:     ResObject,
+			IsArray: true,
+			Fields: []ResponseField{
+				{Field: "filename", Typ: ResString},
+				{Field: "atime", Typ: ResString},
+				{Field: "btime", Typ: ResString},
+				{Field: "mtime", Typ: ResString},
+			},
+		},
+	},
+
+	{
+		Method:     PushFile,
+		Parameters: ParameterFields{"filename", "content", "server"},
+		Response:   Response{Typ: ResString},
+	},
+	{
+		Method:     DeleteFile,
+		Parameters: ParameterFields{"filename", "server"},
+		Response:   Response{Typ: ResString},
+	},
+
+	{
+		Method:     CalculateRam,
+		Parameters: ParameterFields{"filename", "server"},
+		Response:   Response{Typ: ResNumber},
+	},
+
+	{
+		Method:     GetSaveFile,
+		Parameters: ParameterFields{},
+		Response: Response{
+			Typ: ResObject,
+			Fields: []ResponseField{
+				{Field: "identifier", Typ: ResString},
+				{Field: "binary", Typ: ResBoolean},
+				{Field: "save", Typ: ResString},
+			},
 		},
 	},
 }
--- filesync/rpc/definitions/method.go
+++ filesync/rpc/definitions/method.go
@@ -5,13 +5,33 @@ type (
 )
 
 const (
-	MethodError  Method = "error"
-	GetFileNames Method = "getFileNames"
+	MethodError        Method = "error"
+	PushFile           Method = "pushFile"
+	GetFile            Method = "getFile"
+	GetFileMetadata    Method = "getFileMetadata"
+	DeleteFile         Method = "deleteFile"
+	GetFileNames       Method = "getFileNames"
+	GetAllFiles        Method = "getAllFiles"
+	GetAllFileMetadata Method = "getAllFileMetadata"
+	CalculateRam       Method = "calculateRam"
+	GetDefinitionFile  Method = "getDefinitionFile"
+	GetSaveFile        Method = "getSaveFile"
+	GetAllServers      Method = "getAllServers"
 )
 
 func MethodsAsArray() []Method {
 	return []Method{
 		MethodError,
+		PushFile,
+		GetFile,
+		GetFileMetadata,
+		DeleteFile,
 		GetFileNames,
+		GetAllFiles,
+		GetAllFileMetadata,
+		CalculateRam,
+		GetDefinitionFile,
+		GetSaveFile,
+		GetAllServers,
 	}
 }
