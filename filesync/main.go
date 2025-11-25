229519ba09e4abc5d3877fb3c22c46b295bb4681
CTN <info@ctn-originals.com>
2025-11-25 13:27:33 +0100

    main: added a tester function

--- filesync/main.go
+++ filesync/main.go
@@ -10,10 +10,26 @@ func main() {
 	print("\n\n---- FileSync START ----\n")
 	defer print("---- Program END ----")
 
-	// fileContent := handler.GetFileByPath("fileTest.ts")
-	// sanitized := handler.SanitizeFileContent(fileContent)
-	// println(string(sanitized))
+	test()
 
 	go debug.DebugCommandListener()
 	rpc.StartServer(constants.Port)
+}
+
+func test() {
+	// // fileContent := handler.GetFileByPath("fileTest.ts")
+	// // sanitized := handler.SanitizeFileContent(fileContent)
+	// // println(string(sanitized))
+
+	// def := definitions.RPCDefinitions.GetByMethod(definitions.GetFile)
+	// defaultParameters, _ := debug.DefaultParameters[def.Method]
+
+	// jsonRpc := rpc.NewRPC(def.Method, defaultParameters...).String()
+	// jsonMap := handler.JSONToMap([]byte(jsonRpc))
+
+	// println(jsonRpc)
+
+	// for key, val := range *jsonMap {
+	// 	fmt.Printf("%s: (%T) %v\n", key, val, val)
+	// }
 }
