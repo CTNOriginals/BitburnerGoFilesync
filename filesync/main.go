0e32cf3ab3878004db76c7a8f89a1b32fff70a9f
CTN <info@ctn-originals.com>
2025-11-26 07:54:46 +0100

    Improve testing

--- filesync/main.go
+++ filesync/main.go
@@ -4,32 +4,40 @@ import (
 	"filesync/constants"
 	"filesync/debug"
 	"filesync/rpc"
+	"filesync/test"
+	"os"
+)
+
+var (
+	debugMode = false
+	testMode  = false
 )
 
 func main() {
+	parseArgs(os.Args)
+
 	print("\n\n---- FileSync START ----\n")
 	defer print("---- Program END ----")
 
-	test()
+	if testMode {
+		test.DoTest()
+		return
+	}
 
-	go debug.DebugCommandListener()
+	if debugMode {
+		go debug.DebugCommandListener()
+	}
+
 	rpc.StartServer(constants.Port)
 }
 
-func test() {
-	// // fileContent := handler.GetFileByPath("fileTest.ts")
-	// // sanitized := handler.SanitizeFileContent(fileContent)
-	// // println(string(sanitized))
-
-	// def := definitions.RPCDefinitions.GetByMethod(definitions.GetFile)
-	// defaultParameters, _ := debug.DefaultParameters[def.Method]
-
-	// jsonRpc := rpc.NewRPC(def.Method, defaultParameters...).String()
-	// jsonMap := handler.JSONToMap([]byte(jsonRpc))
-
-	// println(jsonRpc)
-
-	// for key, val := range *jsonMap {
-	// 	fmt.Printf("%s: (%T) %v\n", key, val, val)
-	// }
+func parseArgs(args []string) {
+	for _, arg := range args {
+		switch arg {
+		case "--test":
+			testMode = true
+		case "--debug":
+			debugMode = true
+		}
+	}
 }
---
+++ filesync/test/test.go
@@ -0,0 +1,33 @@
+package test
+
+import (
+	"filesync/constants"
+	"filesync/handler"
+	"fmt"
+	"os"
+)
+
+func DoTest() {
+	handler.ForEachFileInDirRecursive(constants.BitburnerRelativePath, func(file os.FileInfo, dir string) {
+		h, m, s := file.ModTime().Clock()
+		fmt.Printf("File %s %s: %d:%d:%d\n", dir, file.Name(), h, m, s)
+	})
+	// info, _ := os.Stat(constants.BitburnerRelativePath + "fileWatch.ts")
+	// println(info.ModTime().Clock())
+
+	// // fileContent := handler.GetFileByPath("fileTest.ts")
+	// // sanitized := handler.SanitizeFileContent(fileContent)
+	// // println(string(sanitized))
+
+	// def := definitions.RPCDefinitions.GetByMethod(definitions.GetFile)
+	// defaultParameters, _ := debug.DefaultParameters[def.Method]
+
+	// jsonRpc := rpc.NewRPC(def.Method, defaultParameters...).String()
+	// jsonMap := handler.JSONToMap([]byte(jsonRpc))
+
+	// println(jsonRpc)
+
+	// for key, val := range *jsonMap {
+	// 	fmt.Printf("%s: (%T) %v\n", key, val, val)
+	// }
+}
