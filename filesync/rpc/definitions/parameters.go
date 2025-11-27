78dd32f06b08c002685faeaf45e09f9a76b7215f
CTN <info@ctn-originals.com>
2025-11-27 10:34:02 +0100

    definitions/parameters: Improve generator function

--- filesync/rpc/definitions/parameters.go
+++ filesync/rpc/definitions/parameters.go
@@ -1,20 +1,22 @@
 package definitions
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // The parameters of a request are always of type string.
 // This type will only hold the fields of any params that need to be passed
 type ParameterFields []string
 
 func (this ParameterFields) Generate(values []string) string {
+	if len(this) != len(values) {
+		fmt.Printf("ParameterFields  (%v) do not match passed in values (%v)", this, values)
+		return "{}"
+	}
+
 	str := ""
 
 	for i, field := range this {
-		if i > len(values)-1 {
-			fmt.Printf("There are not enough values to populate the remaining parameter fields: %v", this[i:])
-			break
-		}
-
 		str += fmt.Sprintf("  \"%s\": \"%s\"", field, values[i])
 
 		if i < len(this)-1 {
