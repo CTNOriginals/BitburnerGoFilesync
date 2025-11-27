6aad279d62cebe7842ebcebdb60d1a819f921e1e
CTN <info@ctn-originals.com>
2025-11-27 11:31:52 +0100

    definitions/method: Added parameter context

--- filesync/rpc/definitions/method.go
+++ filesync/rpc/definitions/method.go
@@ -5,23 +5,46 @@ type (
 )
 
 const (
-	MethodError        Method = "error"
-	PushFile           Method = "pushFile"
-	GetFile            Method = "getFile"
-	GetFileMetadata    Method = "getFileMetadata"
-	DeleteFile         Method = "deleteFile"
-	GetFileNames       Method = "getFileNames"
-	GetAllFiles        Method = "getAllFiles"
+	// Parameters:
+	//   func(filename, content, server)
+	PushFile Method = "pushFile"
+
+	// Parameters:
+	//   func(filename, server)
+	GetFile Method = "getFile"
+
+	// Parameters:
+	//   func(filename, server)
+	GetFileMetadata Method = "getFileMetadata"
+
+	// Parameters:
+	//   func(filename, server)
+	DeleteFile Method = "deleteFile"
+
+	// Parameters:
+	//   func(server)
+	GetFileNames Method = "getFileNames"
+
+	// Parameters:
+	//   func(filename, server)
+	GetAllFiles Method = "getAllFiles"
+
+	// Parameters:
+	//   func(server)
 	GetAllFileMetadata Method = "getAllFileMetadata"
-	CalculateRam       Method = "calculateRam"
-	GetDefinitionFile  Method = "getDefinitionFile"
-	GetSaveFile        Method = "getSaveFile"
-	GetAllServers      Method = "getAllServers"
+
+	// Parameters:
+	//   func(filename, server)
+	CalculateRam Method = "calculateRam"
+
+	GetDefinitionFile Method = "getDefinitionFile"
+	GetSaveFile       Method = "getSaveFile"
+	GetAllServers     Method = "getAllServers"
+	MethodError       Method = "error"
 )
 
 func MethodsAsArray() []Method {
 	return []Method{
-		MethodError,
 		PushFile,
 		GetFile,
 		GetFileMetadata,
@@ -33,5 +56,6 @@ func MethodsAsArray() []Method {
 		GetDefinitionFile,
 		GetSaveFile,
 		GetAllServers,
+		MethodError,
 	}
 }
