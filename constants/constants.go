e87afc43eb10f7d812c36e443b8fe6b58adddf4b
CTNOriginals <info@ctn-originals.com>
2026-01-31 13:50:32 +0100

    MISC: Comment cleanup

--- constants/constants.go
+++ constants/constants.go
@@ -5,15 +5,8 @@ import (
 	"os"
 )
 
-// var Port = "8080"
 var WorkindDirectory, _ = os.Getwd()
-
-// var BitburnerRoot = WorkindDirectory
-// var IncludeFileExt = []string{"js", "ts", "txt"}
 var ConfigFilePath = fmt.Sprintf("%s/%s", WorkindDirectory, "config.toml")
-
-// The time to sleep until the next file scan in miliseconds
-// var FileScanDelay = 100
 
 var Debug = false
 var NoWatcher = false
