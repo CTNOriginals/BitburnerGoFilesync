1c806b0b87ecb6633fe791051d87785a936334d3
CTNOriginals <info@ctn-originals.com>
2026-05-05 17:45:50 +0200

    FUNC: config command

--- commands/entries/config.go
+++ commands/entries/config.go
@@ -1,7 +1,11 @@
 package entries
 
 import (
+	"fmt"
+
 	"github.com/CTNOriginals/BitburnerGoFilesync/commands"
+	"github.com/CTNOriginals/BitburnerGoFilesync/config"
+	"github.com/CTNOriginals/BitburnerGoFilesync/constants"
 )
 
 var conf = commands.Definition{
@@ -18,4 +22,13 @@ func init() {
 }
 
 func config_execute() {
+	fmt.Printf("%s: %v\n", "Port", config.Values.Port)
+	fmt.Printf("%s: %v\n", "WorkindDirectory", constants.WorkindDirectory)
+	fmt.Printf("%s: %v\n", "WorkindDirectory", constants.ConfigFilePath)
+	fmt.Printf("%s: %v\n", "BitburnerRoot", config.Values.Directory)
+	fmt.Printf("%s: %v\n", "IncludeFileExt", config.Values.FilePatterns.Include)
+	fmt.Printf("%s: %v\n", "FileScanDelay", config.Values.FilePatterns.Exclude)
+	fmt.Printf("%s: %v\n", "NoWatcher", constants.NoWatcher)
+	fmt.Printf("%s: %v\n", "NoServer", constants.NoServer)
+	fmt.Printf("%s: %v\n", "KeepAlive", constants.KeepAlive)
 }
