b6990ebc659eb3726a8c3e5c99bdced638f897a9
CTNOriginals <info@ctn-originals.com>
2026-05-12 11:32:04 +0200

    func: input and input list stringers

--- cli/commands/input.go
+++ cli/commands/input.go
@@ -1,6 +1,12 @@
 package commands
 
+import "fmt"
+
 type Input struct {
 	Def   *Definition
 	Value string
+}
+
+func (this Input) String() string {
+	return fmt.Sprintf("%s: %s", this.Def.Name, this.Value)
 }
--- cli/commands/inputList.go
+++ cli/commands/inputList.go
@@ -1,7 +1,26 @@
 package commands
 
+import (
+	"fmt"
+	"strings"
+)
+
 type TInputList []*Input
 
 func (this TInputList) Execute() {
+	fmt.Printf("%s\n", this)
+}
 
+func (this TInputList) String() string {
+	var str strings.Builder
+
+	for i, input := range this {
+		if str.Len() > 0 {
+			str.WriteRune('\n')
+		}
+
+		fmt.Fprintf(&str, "%d: %s", i, input)
+	}
+
+	return str.String()
 }
