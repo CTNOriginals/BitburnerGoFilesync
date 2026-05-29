dd341196e79e45adb9a53f015f482430535c30f6
CTNOriginals <info@ctn-originals.com>
2026-05-29 16:24:50 +0200

    test: update clogger test

--- clogger/tests.go
+++ clogger/tests.go
@@ -1,6 +1,11 @@
 package clogger
 
-import "log"
+import (
+	"log"
+	"time"
+
+	"github.com/CTNOriginals/BitburnerGoFilesync/constants"
+)
 
 func TestLogger() {
 	log.Printf("\n--- CLOGGER TEST ---\n")
@@ -11,7 +16,24 @@ func TestLogger() {
 			PrefixTime |
 			PrefixDate |
 			PrefixFile,
+
+		DefaultState: func() bool {
+			return constants.Debug
+		},
+
+		LogLevelState: MLogLevelState{
+			LogInfo: func() bool {
+				var now = time.Now()
+				return now.Second()%2 == 0
+			},
+			LogError | LogFatal: func() bool {
+				return true
+			},
+		},
 	}
+
 	clog.Info("hello world!")
-	clog.Debugf("Levels: '%v'", clog)
+	clog.Debugf("Levels: '%v'", clog.LogLevelState)
+	clog.Error("something gone wrong!\nyou better fix it...")
+	clog.Fatalf("fat alf done it again: %v", clog)
 }
