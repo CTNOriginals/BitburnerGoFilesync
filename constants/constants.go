20163f6dacd4f8c86b4abb048dee5db818e39315
CTNOriginals <info@ctn-originals.com>
2026-05-30 17:05:22 +0200

    misc: constants add PackageEntryPath

--- constants/constants.go
+++ constants/constants.go
@@ -3,9 +3,15 @@ package constants
 import (
 	"fmt"
 	"os"
+	"runtime"
+	"strings"
 )
 
 var WorkindDirectory, _ = os.Getwd()
+
+// The programs root package entry point
+var PackageEntryPath = "github.com/CTNOriginals/BitburnerGoFilesync"
+
 var ConfigFilePath = fmt.Sprintf("%s/%s", WorkindDirectory, "config.toml")
 
 var Debug = false
@@ -15,3 +21,11 @@ var NoWatcher = false
 var NoServer = false
 
 var KeepAlive = false
+
+func init() {
+	var pc, _, _, _ = runtime.Caller(0)
+	var entryPath = runtime.FuncForPC(pc).Name()
+	var parts = strings.Split(entryPath, "/")
+
+	PackageEntryPath = strings.Join(parts[0:len(parts)-1], "/")
+}
