240a0e935eff1a57301d5db8aa6bf4baf3f3d764
CTNOriginals <info@ctn-originals.com>
2026-05-31 18:41:30 +0200

    impl: clogger cli

--- cli/test.go
+++ cli/test.go
@@ -1,16 +1,15 @@
 package cli
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/CTNOriginals/BitburnerGoFilesync/cli/commands"
 )
 
 func TestCli() {
-	fmt.Printf("%s\n", commands.List.StringRecurse())
+	clog.Infof("Commands:\n%s\n", commands.List.StringRecurse())
 
-	// cliSegments()
+	cliSegments()
 	// cliCommands()
 	// cliHelp()
 
@@ -18,62 +17,59 @@ func TestCli() {
 }
 
 func cliSegments() {
-	fmt.Printf("\n-- Segment Tests --\n")
+	clog.Info("\n-- Segment Tests --\n")
 
 	var tests = []string{
 		"config set \"foo bar\" baz",
 		"\"foo 'baz' bar\" goo 'drap bah'",
-		"\"foo 'baz",
+		// "\"foo 'baz",
 		"something 'like a dog' or cat'",
-		"some weird='edge ca'se` that i hope` ne've`r happens",
+		// "some weird='edge ca'se` that i hope` ne've`r happens",
 		"goo foo='bar ins\\'t baz' but is",
 	}
 
 	for _, line := range tests {
-		fmt.Printf(">> %s\n", line)
+		clog.Infof(">> %s\n", line)
 
 		var segments, err = GetInputSegments(line)
 
 		if err == nil {
-			fmt.Printf("%s\n", strings.Join(segments, "\n"))
+			clog.Messagef("%s\n", strings.Join(segments, "\n"))
 		} else {
-			fmt.Printf("%v\n", err)
+			clog.Errorf("%v\n", err)
 		}
 	}
 }
 
 func cliTester(commands []string) {
 	for _, line := range commands {
-		fmt.Printf(">> %s\n", line)
+		clog.Infof(">> %s\n", line)
 
 		var err = ParseInput(line)
 		if err != nil {
-			fmt.Printf("%v\n", err)
+			clog.Infof("%v\n", err)
 		}
-
-		println("")
 	}
 }
 
 func cliCommands() {
-	fmt.Printf("\n-- Command Tests --\n")
+	clog.Message("\n-- Command Tests --\n")
 
 	cliTester([]string{
-		// "config",
-		// "config list",
-		// "config set Port 1234",
+		"config",
+		"prototype 123",
 	})
 }
 
 func cliHelp() {
-	fmt.Printf("\n-- Help Tests --\n")
+	clog.Message("\n-- Help Tests --\n")
 
 	cliTester([]string{
 		"prototype help",
 		"prototype num help",
 		"prototype 'foo bar' help",
-		"prototype logging/index.ts help",
-		"config set help",
-		"config set Port 1234 help",
+		"prototype foo.js help",
+		// "config set help",
+		// "config set Port 1234 help",
 	})
 }
