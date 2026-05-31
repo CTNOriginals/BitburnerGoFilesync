bcfa3b77e1db295402a2f5dcaedc25c5b8439707
CTNOriginals <info@ctn-originals.com>
2026-05-31 19:48:12 +0200

    refac: websocket clog use default clone

--- websocket/websocket.go
+++ websocket/websocket.go
@@ -1,3 +1,9 @@
 package websocket
 
+import "github.com/CTNOriginals/BitburnerGoFilesync/clogger"
+
+var clog = clogger.Default.Clone(clogger.SClog{
+	Name: "websocket",
+})
+
 var Client = &SClient{}
