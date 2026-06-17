77b4a88b8e47b4818be3065467308d20fa1e4bb5
CTNOriginals <info@ctn-originals.com>
2026-06-17 13:10:01 +0200

    misc(config): validate file patterns

--- config/config.go
+++ config/config.go
@@ -1,12 +1,14 @@
 package config
 
 import (
+	"runtime"
 	"strings"
 
 	"github.com/BurntSushi/toml"
 	"github.com/CTNOriginals/BitburnerGoFilesync/clogger"
 	"github.com/CTNOriginals/BitburnerGoFilesync/constants"
 	ctnfile "github.com/CTNOriginals/CTNGoUtils/v2/file"
+	"github.com/bmatcuk/doublestar/v4"
 )
 
 func init() {
@@ -80,4 +82,17 @@ func Initialize() {
 
 func validateConfigValues() {
 	ValidateBitburnerDirectory(Values.Directory)
+
+	var invalidPatterns = make([]string, 0)
+
+	for _, pattern := range append(Values.FilePatterns.Include, Values.FilePatterns.Exclude...) {
+		if !doublestar.ValidatePathPattern(pattern) {
+			invalidPatterns = append(invalidPatterns, pattern)
+		}
+	}
+
+	if len(invalidPatterns) > 0 {
+		clog.Fatalf("The following file pattens are invalid:\n%s", strings.Join(invalidPatterns, "\n"))
+		runtime.Goexit()
+	}
 }
