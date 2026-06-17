8bb45d4654f01a3cd2d0e7b5f548f54ca3a5aca8
CTNOriginals <info@ctn-originals.com>
2026-06-17 12:07:03 +0200

    func(watcher): Initialize entry with error checks

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -1,6 +1,8 @@
 package watcher
 
 import (
+	"runtime"
+
 	"github.com/CTNOriginals/BitburnerGoFilesync/clogger"
 	"github.com/CTNOriginals/BitburnerGoFilesync/config"
 )
@@ -9,7 +11,33 @@ var clog = clogger.Default.Clone(clogger.SClog{
 	Name: "watcher",
 })
 
+var DirEntry *SEntry
+
 func Initialize() {
 	var dir = config.Values.Directory
 	clog.Debugf("Watcher Initialize, dir: %s", dir)
+
+	var entry = newEntry(dir)
+
+	if entry.infoError != nil {
+		if !entry.Exists() {
+			clog.Fatalf(
+				"The configured entry directory does not exist: %s\n%v",
+				dir, entry.infoError,
+			)
+		} else {
+			clog.Fatalf(
+				"An unknown entry error was returned while initializing the entry directory: %s\n%v",
+				dir, entry.infoError,
+			)
+		}
+
+		runtime.Goexit()
+	}
+
+	DirEntry = entry
+}
+
+func scanner() {
+	// TODO:
 }
