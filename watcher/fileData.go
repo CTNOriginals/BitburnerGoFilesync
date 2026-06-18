13208dcf3cfdde38bea11e753f1fca68961821ef
CTNOriginals <info@ctn-originals.com>
2026-06-18 15:54:32 +0200

    func(watcher): fileData add base functions

--- watcher/fileData.go
+++ watcher/fileData.go
@@ -2,23 +2,47 @@ package watcher
 
 import (
 	"os"
-	"path/filepath"
 )
 
 // A wrapper for [os.FileInfo] to add some fields needed tot track a files state.
 type SFileData struct {
+	Path string
 	os.FileInfo
-
-	dir string
 }
 
-func GetFileData(path string) (SFileData, error) {
+func GetFileData(path string) (*SFileData, error) {
 	var info, err = os.Stat(path)
 
 	var data = SFileData{
+		Path:     path,
 		FileInfo: info,
-		dir:      filepath.Dir(path),
 	}
 
-	return data, err
+	return &data, err
+}
+
+func (this SFileData) GetInfo() (os.FileInfo, error) {
+	return os.Stat(this.Path)
+}
+
+func (this SFileData) Exists() bool {
+	var _, err = this.GetInfo()
+	return !os.IsNotExist(err)
+}
+
+func (this SFileData) IsModified() bool {
+	var info, _ = this.GetInfo()
+	return this.ModTime() != info.ModTime()
+}
+
+func (this *SFileData) Update() error {
+	var info, err = this.GetInfo()
+
+	if err != nil {
+		return err
+	}
+
+	this.FileInfo = info
+
+	return nil
 }
