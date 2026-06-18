1a6ff5246c3212fa00a6a89c8c3ffacd54e70e51
CTNOriginals <info@ctn-originals.com>
2026-06-18 15:13:09 +0200

    refac(watcher): less is more

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -1,7 +1,6 @@
 package watcher
 
 import (
-	"runtime"
 	"time"
 
 	"github.com/CTNOriginals/BitburnerGoFilesync/clogger"
@@ -12,37 +11,9 @@ var clog = clogger.Default.Clone(clogger.SClog{
 	Name: "watcher",
 })
 
-var Entry *SEntry
-
 func Initialize() {
 	var dir = config.Values.Directory
 	clog.Debugf("Watcher Initialize, dir: %s", dir)
-
-	var entry = newEntry(dir)
-
-	if entry.infoError != nil {
-		if !entry.Exists() {
-			clog.Fatalf(
-				"The configured entry directory does not exist: %s\n%v",
-				dir, entry.infoError,
-			)
-		} else {
-			clog.Fatalf(
-				"An unknown entry error was returned while initializing the entry directory: %s\n%v",
-				dir, entry.infoError,
-			)
-		}
-
-		runtime.Goexit()
-	}
-
-	generatePatternPaths()
-	entry.SetPathFilter(filePatternFilter)
-	entry.UpdateChildList()
-
-	Entry = entry
-
-	clog.Debug(entry.StringRecursive())
 }
 
 func StartScanner() {
@@ -56,52 +27,12 @@ func StartScanner() {
 }
 
 func scan() {
-	Entry.Recursive(func(entry *SEntry) {
-		if !entry.IsDirectory() {
-			return
-		}
 
-		for _, child := range entry.children {
-			if child.IsDirectory() || !child.IsModified() {
-				continue
-			}
-
-			if child.Exists() {
-				onFileModify(child)
-			} else {
-				onFileDelete(child)
-			}
-		}
-
-		var newEntries = entry.UpdateChildList()
-
-		for _, child := range newEntries {
-			onFileModify(child)
-		}
-	}, true)
-
-	// time.Sleep(time.Second)
-
-	// Update and clean every entry
-	// Entry.Recursive((*SEntry).Update, true)
-	Entry.Recursive(func(entry *SEntry) {
-		// clog.Debugf("Updating %s", entry.info.Name())
-		entry.Update()
-
-		entry.CleanChildListFunc(func(child *SEntry) bool {
-			child.Update()
-			return child.Exists()
-		})
-	}, true)
-
-	// clog.Message("-- end --")
 }
 
-// NOTE: Same as modify
-// func onFileCreate(entry *SEntry) {}
-func onFileModify(entry *SEntry) {
-	clog.Debugf("TODO: Push file: %s", entry.GetPath())
-}
-func onFileDelete(entry *SEntry) {
-	clog.Debugf("TODO: Delete file: %s", entry.GetPath())
-}
+// func onFileModify(info os.FileInfo) {
+// 	clog.Debugf("TODO: Push file: %s", entry.GetPath())
+// }
+// func onFileDelete(info os.FileInfo) {
+// 	clog.Debugf("TODO: Delete file: %s", entry.GetPath())
+// }
