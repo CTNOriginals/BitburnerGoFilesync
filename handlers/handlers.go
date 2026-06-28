1a904d21e15fa35c830423dee52850ed24c6cbf6
CTNOriginals <info@ctn-originals.com>
2026-06-28 15:08:54 +0200

    impl(handlers/netdef): base netscriptdefinitions download functionality

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,6 +2,12 @@
 // not fit inside its own dedicated package.
 package handlers
 
+import "github.com/CTNOriginals/BitburnerGoFilesync/clogger"
+
+var clog = clogger.Default.Clone(clogger.SClog{
+	Name: "handlers",
+})
+
 var NetscriptDefinitions *SNetscriptDefinitions
 
 func Initialize() {
--- handlers/netscriptDefinitions.go
+++ handlers/netscriptDefinitions.go
@@ -1,12 +1,35 @@
 package handlers
 
-type SNetscriptDefinitions struct {
-}
+import (
+	"path/filepath"
+	"strings"
+
+	"github.com/CTNOriginals/BitburnerGoFilesync/config"
+	"github.com/CTNOriginals/BitburnerGoFilesync/websocket"
+	ctnfile "github.com/CTNOriginals/CTNGoUtils/v2/file"
+)
+
+type SNetscriptDefinitions struct{}
 
 func newNetscriptDefinitions() *SNetscriptDefinitions {
-	return &SNetscriptDefinitions{}
+	var nsdef = &SNetscriptDefinitions{}
+
+	websocket.Client.OnReadySubCallback(nsdef.clientOnConnect)
+
+	return nsdef
 }
 
-func (this SNetscriptDefinitions) clientOnConnect() {
+func (this SNetscriptDefinitions) clientOnConnect() (unsub bool) {
+	// TODO: add a config option to disable this function from running
+	// TODO: add dedicated config field for nsdef destination dir
+	var dest = config.Values.Directory
 
+	clog.Debugf("Downloading NetscriptDefinitions to %s", dest)
+
+	websocket.Client.Socket.GetDefinitionFile(func(result *websocket.Result_GetDefinitionFile) {
+		var content = string(*result)
+		ctnfile.WriteFile(filepath.Join(dest, "NetscriptDefinitions.d.ts"), strings.Split(content, "\n"))
+	})
+
+	return true
 }
